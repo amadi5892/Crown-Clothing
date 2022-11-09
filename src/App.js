85c6867ed0aecce752581016812f0e8d861449d2
amadi5892 <amadi5892@gmail.com>
2022-11-08 21:30:22 -0500

    created router navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 
 import Home from './routes/home/home.component';
+
 
 const App = () => {
   return ( 
   <Routes>
-    <Route path='/' element={<Home />} />
-  </Routes>
+    <Route path='/' element={<Navigation />} >
+      <Route path='/' element={<Home />} />
+    </Route>
+</Routes>
   );
 };
 

d57cf4357e427a87c77882efd6871e5c7a0a8c38
amadi5892 <amadi5892@gmail.com>
2022-11-08 21:47:50 -0500

    setup naviagtion styling and logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Navigation from './routes/navigation/navigation.component';
 
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop now page</h1>
+    </div>
+  );
+}
 
 const App = () => {
   return ( 
   <Routes>
     <Route path='/' element={<Navigation />} >
       <Route path='/' element={<Home />} />
+      <Route path='/shop' element={<Shop />} />
     </Route>
 </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter>                      
   </React.StrictMode>
 );
 
